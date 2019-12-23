51eacb02ec58b2d488b9b8349a93820b5af72d8f
Giuseppe Rosa <giusepper11@gmail.com>
2019-12-23 14:44:21 -0300

    Finalizando API com os filtros

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -17,7 +17,15 @@ class BaseRecipeAttr(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Retornando apenas objetos do usuario"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user) \
+            .order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Criando novo objeto"""
